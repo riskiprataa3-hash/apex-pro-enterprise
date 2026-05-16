888d171bf5aecbd8f6add184d630664b68926063
riskiprataa3-hash <riskiprataa3@gmail.com>
2026-05-16 18:40:01 +0700

    feat: Add ClockPage component for routing

---
+++ src/pages/ClockPage.tsx
@@ -0,0 +1,11 @@
+import DigitalClock from '../components/DigitalClock';
+
+const ClockPage = () => {
+  return (
+    <div className="w-full min-h-screen">
+      <DigitalClock />
+    </div>
+  );
+};
+
+export default ClockPage;
